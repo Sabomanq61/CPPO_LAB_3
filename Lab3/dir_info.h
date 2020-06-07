6782ed619d404e33622b5a0ece65d4932b258091
Sabomanq61 <saba.ukubaev@mail.ru>
2020-06-07 15:36:49 +0700

    Правки DirInfo

--- Lab3/dir_info.h
+++ Lab3/dir_info.h
@@ -1,4 +1,26 @@
-#ifndef DIR_INFO_H
-#define DIR_INFO_H
+#pragma once
 
-#endif // DIR_INFO_H
+#include <map>
+#include <QString>
+#include <QTextStream>
+#include <iostream>
+//Результирующая структура содержит инфу о директории.
+
+struct DirInfo
+{
+    QString path_; // Путь к директории
+    uint64_t totalSize_; // Общий размер директории
+    std::map<QString, double> hash_; //map объектов и их размеров
+
+    void PrintInfo() //Функция вывода имени директории и ее содержимого
+    {
+        QTextStream cout_(stdout);
+
+        cout_ << path_  << " total size = " << totalSize_ << endl << flush;
+
+        for(const auto& it : hash_)
+        {
+            cout_ << it.first << ' ' << it.second << '%' << endl << flush;
+        }
+    }
+};
